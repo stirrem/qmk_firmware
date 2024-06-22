d89b14278f98b4c9c1cc93eb36820ce9c8d503cb
Lynton Ivins <lynton.ivins@proton.me>
2024-06-23 09:07:42 +1200

    added new keyboard design

---
+++ keyboards/handwired/acmac3/keymaps/default/rules.mk
@@ -0,0 +1,4 @@
+
+# BOARD = GENERIC_RP_RP2040     # added as per platform dev
+RGBLIGHT_ENABLE = yes
+WS2812_DRIVER = vendor
---
+++ keyboards/handwired/acmac3/keymaps/via/config.h
@@ -0,0 +1,10 @@
+// Copyright 2024 lynton ivins (@stirrem)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+ #define RGBLIGHT_LAYERS
+ #define WS2812_DI_PIN GP22
+ #define RGBLIGHT_LED_COUNT 15
+ #define DYNAMIC_KEYMAP_MACRO_COUNT 60
+
